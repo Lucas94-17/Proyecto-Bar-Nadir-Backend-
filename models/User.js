11dadcd8f6a5a53f850df40045feaec98dc6154e
Lucas-Arteaga <lucas.arteaga16@gmail.com>
2023-07-11 12:46:14 -0300

    feature models-users

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const { model, Schema } = mongoose
+
+//! Creamos la estructura de un tipo de documento (users)
+const userSchema = new Schema({
+	id: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	userName: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		enum: ["admin", "client"],
+		required: true,
+	},
+})
+
+//! Como primer parametro, va el nombre de la coleccion
+module.exports = model("Users", userSchema)
