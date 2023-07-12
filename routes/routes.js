93ef7782c19afe9d7ec23685620166745b048918
Lucas-Arteaga <lucas.arteaga16@gmail.com>
2023-07-12 20:04:42 -0300

    feature conexion-base-de-datos

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,8 @@
 const express = require ("express")
-
+// aca se importa las funciones declaradas en los controladores de User
+const {
+    deleteUser,
+    readUser,
+    readUsers,
+    updateUser,
+} = require("../controllers/User")
