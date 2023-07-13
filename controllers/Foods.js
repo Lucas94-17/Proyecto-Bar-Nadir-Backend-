df68ecfe9ce6b186fa129f48a275bb5cabffebd3
Lucas-Arteaga <lucas.arteaga16@gmail.com>
2023-07-13 19:01:42 -0300

    feature controllers-foods-terminated

--- controllers/Foods.js
+++ controllers/Foods.js
@@ -1,6 +1,6 @@
 const uuid = require("uuid")
 
-const CourseModel = require("../models/Food")
+const FoodModel = require("../models/Food")
 const UserModel=require("../models/User")
 const {text} = require("express")
 
@@ -20,7 +20,7 @@ async function createFood(req,res){
     try{
         UserModel.findOne({id : creator_id}).then(response =>{
             if(response?.id === "admin"){
-                const data = new CourseModel({
+                const data = new FoodModel({
                     id : uuid.v4(),
                     title,img,detail,price,category,
                 })
@@ -38,6 +38,72 @@ async function createFood(req,res){
     }
 }
 
-async function deleteCourse(req,res){
+async function deleteFood(req,res){
     const {id} = req.params
+
+    try{
+        FoodModel.deleteOne({id}).then(response => {
+            if(response.deletedCount){
+                res.status(200).json({
+                    message: `El curso con ${id} fue borrado exitosamente.`,
+                })
+            }else{
+                res.status(200).json({
+                    message:`No se ha encontrado el curso : ${id}`,
+                })
+            }
+        })
+    }catch(error){
+        res.status(400).json({message:error.message})
+    }
+}
+
+
+async function updateFood(req,res){
+    const {id_food , modify} = req.body
+
+    try{
+        FoodModel.finOneAndUpdate({ id : id_food}, modify).then(
+            response =>{
+                if(response.id){
+                    res.status(200).json({
+                        message: `El curso con id ${response.id} fue editado exitosamente.`,
+                        data : res.body,
+                    })
+                }
+            }
+        )
+    }catch (error){
+        res.status(400).json({message:error.message})
+    }
+}
+
+async function searchFoods(req,res){
+    const {q} = req.query
+    try{
+        await FoodModel.find(
+            {
+                $text : {$search :q},
+            },
+            {
+                score : {$meta : "textScore"}
+            },
+        )
+        .sort({
+            score : {$meta :"textScore"},
+        })
+        .then(response =>{
+            res.status(200).json({data:response})
+        })
+    }catch{
+        res.status(400).json({message:error.message})
+    }
+}
+
+module.exports = {
+    readFoods,
+	createFood,
+	deleteFood,
+	updateFood,
+	searchFoods,
 }
