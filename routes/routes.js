fba02f6aa2647d6b02caba66115029c170ade9a5
Lucas-Arteaga <lucas.arteaga16@gmail.com>
2023-07-15 23:55:35 -0300

    feature routes

--- routes/routes.js
+++ routes/routes.js
@@ -20,7 +20,15 @@ const {login,register} = require("../controllers/Auth")
 router.get("/read-users",readUsers)
 
 router.post("/create-user",validateCreate,register)
-
+router.delete("/delete-user/:id", validateDelete, deleteUser)
+router.put("/update-user", updateUser)//peticion get para el paginado al leer los usuarios
+router.get(
+	"/read-users-paginated",
+	validateGetWithQueryStrings,
+	readUsers
+)
 
 router.post("/login" ,login)
+
+
 module.exports = router
