6c278c4cf45b6850cabef6589150568c34cb2007
Lucas-Arteaga <lucas.arteaga16@gmail.com>
2023-07-16 00:13:43 -0300

    feature validators-food

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,11 @@
 const express = require ("express")
 //aca definimos router 
 const router = express.Router()
+
 // aca se importa las funciones declaradas en los controladores de User
+
+//       Usuario!
+
 const {
     deleteUser,
     readUser,
@@ -17,6 +21,7 @@ const {
 
 const {login,register} = require("../controllers/Auth")
 
+
 router.get("/read-users",readUsers)
 
 router.post("/create-user",validateCreate,register)
@@ -28,7 +33,38 @@ router.get(
 	readUsers
 )
 
+router.get("/read-users", verifyToken, verifyIsAdmin, readUsers)
+router.get("/read-user/:id", verifyToken, readUser)
+
 router.post("/login" ,login)
 
+//      Middlewares : 
 
+const { verifyToken } = require("../middlewares/verifyToken");
+const { verifyIsAdmin } = require("../middlewares/VerifyIsAdmin")
+
+
+//      Food !!
+const {
+	validateCreateCourse,
+} = require("../middlewares/validators/course")
+
+const {
+        readFoods,
+        createFood,
+        deleteFood,
+        updateFood,
+        searchFoods,
+} = require("../controllers/Course")
+
+//Leer la comida que hay en la base de datos
+router.get("/read-foods", readFoods)
+
+router.post(
+	"/create-food",
+	validateCreateCourse,
+	verifyToken,
+	verifyIsAdmin,
+	createFood
+)
 module.exports = router
